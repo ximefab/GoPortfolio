d3b362370a0cbf52b0087e0adc62ee5453131f28
Ximena Fabian <ximenafabian@outlook.com>
2022-10-17 01:13:22 -0500

    frontend changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 
-      <div className="SignInDiv">
+
+<div className='textcolor'>
+<nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">Admins only</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>  
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+      <div className='SignInDiv'>
       <main className="form-signin">
         <form>
           <h1 className='h3 mb-3 fw-normal'> please sign in!</h1>
-          <input type="email" id="inputEmail" className='form-control' placeholder='Email address' required autoFocus/> 
+          <input type="email" id="inputEmail" className='form-control' placeholder='Email address' required/> 
           <input type="password" id="inputPassword" className="form-control" placeholder="password" required/>
           <button className='w-100 btn btn-lg btn-primary' type="submit">sign in</button>
         </form>
@@ -18,6 +73,10 @@ function App() {
 
 
       </div>
+
+
+
+
 
 
     </div>
