f53b0454f96cfc23e660e42dcd95985df9f89fb3
Maikayla Brunner <maikayla.brunner25@gmail.com>
2022-10-26 14:25:55 -0500

    added comment

--- main.go
+++ main.go
@@ -12,39 +12,38 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-
 func main() {
 
-    //connecting the database
-    client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://root:rootroot@cluster0.b9lboem.mongodb.net/401db?retryWrites=true&w=majority"))
-	
-    //handling errors
-    if err != nil {
-		log.Fatal(err)
-	}
-    //after 10 seconds the session will be closed
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	//establishing connection error
-    err = client.Connect(ctx)
+	//connecting the database
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://root:rootroot@cluster0.b9lboem.mongodb.net/401db?retryWrites=true&w=majority"))
+
+	//handling errors
 	if err != nil {
 		log.Fatal(err)
 	}
-    //disconnection
+	//after 10 seconds the session will be closed
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	//establishing connection error
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//disconnection
 	defer client.Disconnect(ctx)
 
-    //ping / errors
-    err = client.Ping(ctx, readpref.Primary())
-    if err != nil {
-        log.Fatal(err)
-    }
+	//ping / errors
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    databases, err := client.ListDatabaseNames(ctx, bson.M{})
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(databases)
+	//lists databases as a string
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
 
-}//end main func
-
+} //end main func
 
 //Maikayla!
